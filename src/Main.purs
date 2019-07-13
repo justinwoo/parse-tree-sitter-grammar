a149d351cedcd22f444a97b9f4aca66827ed5fb4
justinwoo <moomoowoo@gmail.com>
2019-07-13 23:29:52 +0300

    handle anonymous node

--- src/Main.purs
+++ src/Main.purs
@@ -9,16 +9,18 @@ import Effect.Class.Console (log)
 import Grammar (readRulesFromGrammarJSON')
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Processing (ruleToContent)
+import Processing (deferenceAnonymous, ruleToContent)
 
 main :: Effect Unit
 main = Aff.launchAff_ do
   json <- readTextFile UTF8 "grammar.json"
   rules <- readRulesFromGrammarJSON' json
   let contents = ruleToContent <$> rules
-  traverse_ print contents
+  traverse_ (print (deferenceAnonymous contents)) contents
   where
-    print r = do
+    print deref r' = do
+      -- let r = deref r'
+      let r = r'
       log r.name
       log $ show r.value
       log ""
--- src/Processing.purs
+++ src/Processing.purs
@@ -2,12 +2,18 @@ module Processing where
 
 import Prelude
 
+import Data.Array as Array
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
+import Data.Identity (Identity(..))
 import Data.List (List(..), (:))
 import Data.List as List
-import Data.String (toUpper)
+import Data.Maybe (Maybe(..), isJust)
+import Data.Newtype (un)
+import Data.String (Pattern(..), toUpper)
 import Data.String.CodeUnits (singleton, toCharArray)
+import Data.String.CodeUnits (stripPrefix) as String
+import Data.Traversable (traverse)
 import Grammar (Rule, RuleType(..))
 
 -- translated from rule type, for what the contents should be
@@ -38,16 +44,29 @@ instance showRuleContent :: Show RuleContent where show x = genericShow x
 derive instance eqRuleContent :: Eq RuleContent
 derive instance ordRuleContent :: Ord RuleContent
 
+immediate :: forall f. Applicative f => (RuleContent -> f RuleContent) -> RuleContent -> f RuleContent
+immediate f r = case r of
+  LiteralValue -> pure LiteralValue
+  SyntaxValue -> pure SyntaxValue
+  Reference string -> pure $ Reference string
+  Choice xs -> Choice <$> traverse f xs
+  Repeat x -> Repeat <$> f x
+  Repeat1 x -> Repeat1 <$> f x
+  Sequence xs -> Sequence <$> traverse f xs
+
 type RuleWithContent =
   { name :: String
   , value :: RuleContent
+  -- the name begins with _, so it is not a real node
+  , isAnonymous :: Boolean
   }
 
 ruleToContent :: Rule -> RuleWithContent
-ruleToContent r = { name, value }
+ruleToContent r = { name, value, isAnonymous }
   where
     name = renameToCamelCase r.name
     value = fromRuleType r.value
+    isAnonymous = isJust $ String.stripPrefix (Pattern "_") name
 
 fromRuleType :: RuleType -> RuleContent
 fromRuleType ruleType = case ruleType of
@@ -64,6 +83,16 @@ fromRuleType ruleType = case ruleType of
   STRING _ -> SyntaxValue
   SYMBOL { name } -> Reference $ renameToCamelCase name
   TOKEN _ -> LiteralValue
+
+deferenceAnonymous :: Array RuleWithContent -> RuleWithContent -> RuleWithContent
+deferenceAnonymous xs rwc = rwc { value = deref rwc.value }
+  where
+    deref :: RuleContent -> RuleContent
+    deref r = un Identity case r of
+      Reference name
+        | Just x <- Array.find (eq name <<< _.name) xs
+        , x.isAnonymous -> pure $ deref x.value
+      _ -> immediate (pure <<< deref) r
 
 renameToCamelCase :: String -> String
 renameToCamelCase str = result
